543beafd060f592fb1aa56628e4fc4c4f2768848
Taylor Walker <79953691+tayynicc@users.noreply.github.com>
2021-03-08 11:33:08 -0700

    Update how-to-learn.js

--- week1/d1-expressions/code-it-out/how-to-learn.js
+++ week1/d1-expressions/code-it-out/how-to-learn.js
@@ -2,14 +2,15 @@
 What are the two 'types' of learning we alternate
 between here at AppAcademy? What are three examples 
 of each?
+Active and Passive learning 
 
-1. 
-  1. 
-  2. 
-  3. 
-2. 
-  1.
-  2.
+1. Active
+  1. writing code 
+  2. actively dedugging 
+  3. makeing websites/ applictaitons. Implementing code learned
+2.  Passive
+  1. attending lectures
+  2. researching concepts
   3.
 
 What is Desirable Difficulty?
